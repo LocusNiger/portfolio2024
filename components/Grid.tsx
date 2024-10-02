cc980cc139b6e5db756612456f52176178c288b4
Iván Sánchez <sanchezivan.798@gmail.com>
2024-10-02 09:00:01 -0300

    Feat: GRID + assets

---
+++ components/Grid.tsx
@@ -0,0 +1,24 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {/* Mapea el arreglo gridItems donde se encuentra la información a mostrar */}
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,6 +26,7 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          {/* Cambiar texto */}
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic web magic with Next.js
           </h2>
