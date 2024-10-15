b15fffa4a67b394c59550bdd9e49618874900746
Iván Sánchez <sanchezivan.798@gmail.com>
2024-10-14 21:50:45 -0300

    Feat: bento grid terminado

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,16 +6,31 @@ const Grid = () => {
     <section id="about">
       <BentoGrid>
         {/* Mapea el arreglo gridItems donde se encuentra la información a mostrar */}
-        {gridItems.map((item) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            className={item.className}
-            img={item.img}
-          />
-        ))}
+        {/* Cada item a mostrar en el grid cuenta con todos estos datos, inclusive las clases a pasar */}
+        {gridItems.map(
+          ({
+            id,
+            title,
+            titleClassName,
+            description,
+            className,
+            img,
+            imgClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              titleClassName={titleClassName}
+              imgClassName={imgClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
